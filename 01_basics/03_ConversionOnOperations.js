e15b3e4cfbda27310205af2803571193a8197a9c
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-06 18:07:38 +0530

    conversion done

---
+++ 01_basics/03_ConversionOnOperations.js
@@ -0,0 +1,26 @@
+let score = "99abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//convert anything to number by 'Number' class.
+let ValueInNumber = Number(score); 
+
+// console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+
+//note -->
+/* 'Number'
+"33" => 33
+"232sfd" => NaN
+true => 1
+null => 0
+*/
+
+let isLoggedIn = "";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+//1 => true;  0 => false
+//"" => false;
+//"aman" => true
