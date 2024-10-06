b5ae9e2bf9be71d687fed397fa8bdda091e0a4fe
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-06 10:19:34 +0000

    variables done

---
+++ 01_basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId  = 12213213;
+let accountEmail = "mail.com";
+var accountPassword = "6674";
+accountCity = "Delhi";
+
+let accountState;
+// accountId = 83434;  can not assign to cost variable again.
+
+accountEmail = "shuisajsbjs";
+accountPassword = "e23232";
+accountCity = "Jaipur";
+console.log(accountId);
+
+/*
+try to avoid 'var' instead use let or cosnt.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
