88042915bc0add06c79fd401e67d993cc1248488
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-06 16:33:24 +0530

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all the js code as newer version.
+
+console.log(3 + 3);
+console.log("Aman");
+
+let name = "Aman"; //string
+let age = 22; //number
+let isLoggedIn = false //boolean
+//bigint
+// null -> standalone value (it is an object)
+//undifined
+//symbol-> uniqueness
+
+//objects-->
+console.log(typeof null);
