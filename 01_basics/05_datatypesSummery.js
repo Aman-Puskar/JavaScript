b458ba893fd5c62ff209036261b8170fe264f874
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-06 21:09:25 +0530

    stack and heap memory

--- 01_basics/05_datatypesSummery.js
+++ 01_basics/05_datatypesSummery.js
@@ -13,10 +13,20 @@ const nextid = Symbol(121);
 // console.log(id === nextid);  //give false becz id and nextid are unique due to 'Symbol';
 
 const bigInt = 4803203920390293230n; //add 'n' it indicates that the number as bigint;
-console.log(bigInt);
-console.log(typeof bigInt);
+// console.log(bigInt);
+// console.log(typeof bigInt);
 
 let myFunc = function () {
     console.log("this is in the function");
 }
-console.log(typeof myFunc);
+// console.log(typeof myFunc);
+
+
+//****************************** Memory************
+/*
+stack (primitive) ,heap (no  primitive)
+-> In stack memory copy of the original is given ,if any change in copy does not affect the original one.
+-> In heap memory variable points to the same objects , if any change is done by any of the variable pointing to the same
+object will change the main object and result will affect to the every variable pointing to same objects.
+
+*/
