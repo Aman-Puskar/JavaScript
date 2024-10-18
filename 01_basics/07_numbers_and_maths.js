b11f298b3af0d1f0d39fcc0513a3925a5c34cf4b
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-10-18 23:02:05 +0530

    maths and numbers done

---
+++ 01_basics/07_numbers_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(400);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //number of digits after the decimal
+
+const otherNumber = 4348.847747;
+
+// console.log(otherNumber.toPrecision(5)); //return the value to precised digits first give the privority to the digits before the decimal.(round off to the precised value;)
+
+const hundred = 10000000;
+// console.log(hundred.toLocaleString('en-IN')); //return the values with the commas so the value can be readed easily and initially it add commas as per the US currency.
+
+
+
+//******************************************************** MATHS  *******************************************/
+// console.log(Math);
+// console.log(Math.abs(-343));
+// console.log(Math.round(44.232));
+
+
+//Math.random() will always give values from 0 to 1;
+console.log(Math.round(((Math.random()*100) + 1)));
+
+const min = 30;
+const max = 50;
+console.log(Math.floor(((Math.random() * (max - min) + 1)) + min));
