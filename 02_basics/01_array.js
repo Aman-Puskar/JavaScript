970d26ac8fe5497284c360500b8293f98df19b8a
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-11-10 23:22:45 +0530

    arrays basics done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,37 @@
+ const arr = [1,2,3,4,5,6,7]; //elements can be of different datatypes.
+//it is resizeable 
+// const newArr = ["apple" , "banana"];/
+// const arr2  = new Array(1, 3, 3, 4, 5);
+
+// console.log(arr[0]);
+
+//array mathods;
+
+// arr.push(787); //add at last;
+// arr.pop() //remove from last;
+// arr.unshift(989);//add at first and shift all the elements;
+// arr.shift();  //remove tha first element and shift rest of the elements;
+// console.log(arr);
+// console.log(arr.includes(9)); //return boolean;
+// console.log(arr.indexOf(88));    
+
+// const myarr = arr.join(); //array as a string;
+// console.log(myarr);
+// console.log(typeof(myarr));
+
+// slice and splice;
+console.log("A" , arr);
+const ar1 = arr.slice(0,4);
+console.log(ar1);
+console.log("B" , arr);
+
+const ar2 = arr.splice(0,4);
+console.log(ar2);
+console.log("C" , arr);
+
+
+
+
+
+
+
