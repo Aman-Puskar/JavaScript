ef664b92be8a4a79b55e6fa3ea9462c8fa26beea
Aman-Puskar <amanpuskaremg25@gmail.com>
2024-12-01 23:01:08 +0530

    forEach , filter, map

---
+++ 05_loops/04_moreAboutForEachLoop.js
@@ -0,0 +1,34 @@
+const ar = ["apple", "banana", "guava", "oranges"];
+
+
+
+//forEach does not return the values
+// const val = ar.forEach((fruits) => {
+//     // console.log(fruits);
+//     return fruits
+    
+// })
+// console.log(val);  //this will not return anything;
+
+
+
+//here filter will the values;
+// const nums = [1, 2, 3, 4, 5, 6, 7];
+
+// const returnValue = nums.filter((num) => num > 5); 
+// console.log(returnValue)
+
+
+
+
+//map
+
+// const arr = [1, 2,32,3,23,2,32,323,23,23];
+// const returnNum = arr.map((num) => num + 10);
+// console.log(returnNum);
+
+// map().map().filter();
+
+
+
+//reduce;
